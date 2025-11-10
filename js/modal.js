d8e43c746007a533a65d70eaed1f3ad222a2928c
Maks Melnyk <maks.melnik1611@gmail.com>
2025-11-10 15:40:43 +0200

    implement Unit 18. Adaptivity

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnsOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnsClose = document.querySelectorAll('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnsOpen.forEach(btn => btn.addEventListener('click', toggleModal));
+modalBtnsClose.forEach(btn => btn.addEventListener('click', toggleModal));
